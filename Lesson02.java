fef706ae0a8f877b6af1b90200e111cba571b63c
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-25 16:24:58 +0530

    Variables

---
+++ Lesson02.java
@@ -0,0 +1,9 @@
+public class Lesson02 {
+    public static void main(String[] args) {
+        // Declare Variables.
+        int myNum = 15;
+        String name = "Rizwan";
+        System.out.println(myNum);
+        System.out.println(name);
+    }
+}
