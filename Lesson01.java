2a8ba16b8358f710c18f7982132227fc6bd5b3fd
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-25 15:28:11 +0530

    Getting Started

--- Lesson01.java
+++ Lesson01.java
@@ -3,5 +3,16 @@
 public class Lesson01 {
     public static void main(String[] aStrings) {
         System.out.println("Hello World");
+        
+        
+        
+// Try to Print more values at a single class.
+        System.out.println("I am Rizwan");
+        System.out.println("I am the Best");
+
+// Try to print in same line by print() method.
+        System.out.print("Next line printing above this stetement .");
+        System.out.print("Now check it it's really working");
+        
     }
 }
