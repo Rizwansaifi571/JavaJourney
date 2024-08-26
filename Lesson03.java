f3d226731515310d520662015646ddeaa78adaa8
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-26 12:04:27 +0530

    Data Types

---
+++ Lesson03.java
@@ -0,0 +1,24 @@
+public class Lesson03 {
+    public static void main(String[] args) {
+        
+        // Integer type data type.
+
+        // byte
+        byte bytnum = 105;
+        System.out.println(bytnum);
+        
+        // short
+        short sortnum = 5000;
+        System.out.println(sortnum); 
+
+        // int
+        int intnum = 2147483647;
+        System.out.println(intnum);
+        
+        // long
+        long longnum = 15000000000L;
+        System.out.println(longnum);
+
+
+            }
+}
