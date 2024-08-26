5a1d072d23f1afc9b2a822e5c5b02fc941b2c4d9
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-26 12:44:36 +0530

    Type Casting And Operator

---
+++ Lesson04.java
@@ -0,0 +1,18 @@
+public class Lesson04 {
+    public static void main(String[] args) {
+        
+        // Widening Casting
+        int myInt = 9;
+        double mydouble = myInt;
+        System.out.println(myInt);
+        System.out.println(mydouble);
+
+        // Narrowing Casting
+        double d1 = 9.78d;
+        int int1 = (int) d1;
+        System.out.println(d1);
+        System.out.println(int1);
+
+        
+    }
+}
