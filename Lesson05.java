5abc30b7348caa989e8a5c7c5384b3135bd9eab9
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-27 16:45:56 +0530

    Strings

---
+++ Lesson05.java
@@ -0,0 +1,8 @@
+public class Lesson05 {
+    public static void main(String[] args) {
+        // String example
+        String greeting = "Hello";
+        System.out.println(greeting);
+
+    }
+}
