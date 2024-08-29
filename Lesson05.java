8a5d3facce8aa90bd4179aab2dc1b4d8778b318b
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-29 17:03:41 +0530

    Strings

--- Lesson05.java
+++ Lesson05.java
@@ -20,5 +20,29 @@ public class Lesson05 {
         String lastname = "Saifi";
 
         System.out.println(firstname.concat(lastname));
+
+
+
+        // Escape Sequence character
+
+        String ex1 = "This is Rizwan\'s Notes.";
+        System.out.println(ex1);
+        String ex2 = "This is Rizwan\"s Notes.";
+        System.out.println(ex2);
+        String ex3 = "This is Rizwan\\s Notes.";
+        System.out.println(ex3);
+        String ex4 = "This is Rizwan\ns Notes.";
+        System.out.println(ex4);
+        
+        String ex5 = "This is Rizwan\rs Notes.";
+        System.out.println(ex5);
+        String ex6 = "This is Rizwan\bs Notes.";
+        System.out.println(ex6);
+        String ex7 = "This is Rizwan\fs Notes.";
+        System.out.println(ex7);
+        String ex8 = "This is Rizwan\ts Notes.";
+        System.out.println(ex8);
+
+
     }
 }
