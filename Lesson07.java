60799e7f30f7e63bac2a2ce739b1df15a947bdba
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-08-30 18:26:27 +0530

    Conditional Statement

---
+++ Lesson07.java
@@ -0,0 +1,21 @@
+public class Lesson07 {
+    public static void main (String[] args) {
+        
+        // only if statment
+        if (18>15) {
+            System.out.println("18 is greater than 15");
+        }
+
+        // with else statement
+        int age = 24;
+        if (age > 18) {
+            System.out.println("You are adult");
+        }
+        else {
+            System.out.println("you are not adult");
+        }
+
+        
+
+    }
+}
