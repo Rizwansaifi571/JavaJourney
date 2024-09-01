1cca9cee766b5eb0aadd70a67b540822351b7c00
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-01 14:04:08 +0530

    Strings

--- Lesson08.java
+++ Lesson08.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class Lesson08 {
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
-        System.out.print("Enter a day number (1-7) : ");
+
+        // First switch case: Day of the week
+        System.out.print("Enter a day number (1-7): ");
         int day = input.nextInt();
-        // Switch case
+
+        // Switch case to print the corresponding day
         switch (day) {
             case 1:
                 System.out.println("Monday");
@@ -30,7 +31,29 @@ public class Lesson08 {
             case 7:
                 System.out.println("Sunday");
                 break;
+            default:
+                System.out.println("Invalid day number. Please enter a number between 1 and 7.");
+                break;
         }
+
+        // Second switch case: Party day input
+        System.out.print("\nEnter a day number for the party (1-7): ");
+        day = input.nextInt();
+
+        // Switch case with default parameter
+        switch (day) {
+            case 6:
+                System.out.println("Saturday is a nice choice!");
+                break;
+            case 7:
+                System.out.println("Sunday might be busy.");
+                break;
+            default:
+                System.out.println("Weekdays can be tough for parties. Weekends are better!");
+                break;
+        }
+
+        // Close the scanner
+        input.close();
     }
-    
 }
