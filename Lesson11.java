4d1fe1f9c25129c88b03c0e389698069b0949ad9
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-01 20:44:50 +0530

    Array

--- Lesson11.java
+++ Lesson11.java
@@ -17,10 +17,7 @@ public class Lesson11 {
         // Array length
         System.out.println(cars.length);
 
-
-        
         // loop through an array
-        
         // using for loop
         for (int i = 0; i < cars.length; i++) {
             System.out.println(cars[i]);
@@ -30,6 +27,38 @@ public class Lesson11 {
         for (String i : cars) {
             System.out.println(i);
         }
+    
+    
+        
+        // Multi-Dimensional Arrays
+        int[][] num = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(Arrays.deepToString(num));
+
+        // access elements
+        System.out.println(num[1][1]);
+
+        // change element
+        num[1][2] = 7;
+        System.out.println(Arrays.deepToString(num));
+
+        // Loop through multi-dimensional array
+        // using for loop
+        for(int i = 0; i < num.length; i++) {
+            for(int j = 0; j <= num.length; j++) {
+                System.out.print(num[i][j] + " ");
+            }
+        }
+        System.out.println();
+
+        // using for each loop
+        for (int[] row : num) {
+            for (int i : row) {
+                System.out.print(i + " ");
+            }
+        }
+
     }
+
+
     
 }
