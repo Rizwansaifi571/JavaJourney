c05bad4ca69552af4e167db25603e331b1eb8b6d
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-02 23:14:47 +0530

    Java Method

--- Lesson12.java
+++ Lesson12.java
@@ -1,10 +1,20 @@
 public class Lesson12 {
     // First Function
-    static void myMethod() {
+    static void myMethod1() {
         System.out.println("I am the Best!");
     }    
-    public static void main (String[] args) {
-        myMethod();
+
+    // Function with parameter
+    static void myMethod2(String fname, int age) {
+        System.out.println(fname + " is " + age);
+    }
+
+
+
+    public static void main(String[] args) {
+        myMethod1();
+        myMethod2("Rizwan", 20);
+        myMethod2("Jai", 21);
     }
 
 }
