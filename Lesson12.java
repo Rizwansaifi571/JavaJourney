f3e74590cd92de0ea87c48086f2823085fef6374
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-02 21:25:46 +0530

    Java Method

---
+++ Lesson12.java
@@ -0,0 +1,10 @@
+public class Lesson12 {
+    // First Function
+    static void myMethod() {
+        System.out.println("I am the Best!");
+    }    
+    public static void main (String[] args) {
+        myMethod();
+    }
+
+}
