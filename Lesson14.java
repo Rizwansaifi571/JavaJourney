642f43d811c68c9a04f1e0bfecbaa41011f944a2
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-04 00:16:44 +0530

    Java Oops(Classes and objects)

---
+++ Lesson14.java
@@ -0,0 +1,9 @@
+public class Lesson14 {
+    int x = 5;
+    public static void main(String[] args) {
+        Lesson14 myobj1 = new Lesson14();
+        Lesson14 myobj2 = new Lesson14();
+        System.out.println(myobj1.x);
+        System.out.println(myobj2.x);
+    }
+}
