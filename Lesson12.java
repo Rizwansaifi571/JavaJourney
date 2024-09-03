6ea7e6f6d6829a937e0b5c583351fe510d53c2c7
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-03 22:26:43 +0530

    Java Method

--- Lesson12.java
+++ Lesson12.java
@@ -26,12 +26,25 @@ public class Lesson12 {
         return x + y;
     }
 
+
+    // Method Overloading
+    static int plusMethod (int x, int y) {
+        return x + y;
+    }
+    static double plusMethod (double x, double y) {
+        return x + y;
+    }
+
     public static void main(String[] args) {
         myMethod1();
         myMethod2("Rizwan", 20);
         myMethod2("Jai", 21);
         myMethod3(21);
         System.out.println(myMethod4(5, 3));
+        int myNum1 = plusMethod(8, 5);
+        double myNum2 = plusMethod(4.3, 6.26);
+        System.out.println(myNum1 + "\n" + myNum2);
+
     }
 
     
