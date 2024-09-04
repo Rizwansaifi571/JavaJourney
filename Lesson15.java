632b41f4f446b122701e65efe9bc7ca0a12db7a3
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-04 21:02:35 +0530

    Java Oops(Class Attributes)

---
+++ Lesson15.java
@@ -0,0 +1,10 @@
+public class Lesson15 {
+    // Class Attributes
+    int x = 5;
+    public static void main(String[] args) {
+        Lesson15 myObj = new Lesson15();
+        System.out.println(myObj.x);
+        myObj.x = 40;
+        System.out.println(myObj.x);
+    }
+}
