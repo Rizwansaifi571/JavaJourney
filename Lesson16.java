89ef34a13145e27784626b387c772f791f5e8628
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-09-04 21:44:27 +0530

    Java Oops(Class Method)

--- Lesson16.java
+++ Lesson16.java
@@ -7,6 +7,17 @@ public class Lesson16 {
     public void myPublicMethod() {
         System.out.println("Public method must be called my creating objects");
     }
+
+
+
+    // Second Method
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
+    }
+    public void speed(int maxspeed) {
+        System.out.println("Maximum Spees is : " + maxspeed);
+    }
+
     // Main method
     public static void main(String[] args) {
         myStaticMethod();
@@ -15,5 +26,14 @@ public class Lesson16 {
         Lesson16 myObj = new Lesson16();
         myObj.myPublicMethod();
         // myObj.myStaticMethod(); warning occur : The static method myStaticMethod() from the type Lesson16 should be accessed in a static way
+
+
+        Lesson16 speedObj = new Lesson16();
+        speedObj.fullThrottle();
+        speedObj.speed(150);
     }
+
+
+
+    
 }
