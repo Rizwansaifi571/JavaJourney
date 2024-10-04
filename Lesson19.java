df34a824615370c44a8853dd9bd0c1123d7842d5
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-10-04 22:19:57 +0530

    Encapsulation

---
+++ Lesson19.java
@@ -0,0 +1,14 @@
+public class Lesson19 {
+    private String name;
+
+    // Getter
+    public String GetName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
+
