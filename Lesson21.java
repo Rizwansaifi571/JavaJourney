e7da5d7203a3705b49523220cfbb55aa857ee8e3
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-10-05 12:08:55 +0530

    Inheritance

---
+++ Lesson21.java
@@ -0,0 +1,10 @@
+public class Lesson21 {
+    String brand = "Ford";
+    public void honk() {
+        System.out.println("Tunt, tuut!!");
+    }
+}
+
+class Car extends Lesson21 {
+    String ModelName = "Mustang";
+}
