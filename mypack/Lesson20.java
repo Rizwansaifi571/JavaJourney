bc3d764b022d77737aeb852f9c4ef92f7852334d
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-10-05 11:40:28 +0530

    User_defined Package

---
+++ mypack/Lesson20.java
@@ -0,0 +1,13 @@
+package mypack;
+public class Lesson20 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
+
+
+
+// javac -d . mypack\Lesson20.java
+
+
+// java mypack.Lesson20
