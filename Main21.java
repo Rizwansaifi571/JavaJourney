f27bd6028ccced65d3f76800c4d0e5c41d4f1a55
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-10-20 19:48:46 +0530

    Concepts

--- Main21.java
+++
@@ -1,7 +0,0 @@
-public class Main21 {
-    public static void main(String[] args) {
-        Car mycar = new Car();
-        mycar.honk();
-        System.out.println(mycar.brand + " " + mycar.ModelName);
-    }
-}
