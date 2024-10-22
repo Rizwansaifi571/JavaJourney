6f7c84b890e7ffc0c6a4922520e5d679d6edd6ac
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-10-22 19:49:51 +0530

    Concepts

--- Main19.java
+++
@@ -1,7 +0,0 @@
-public class Main19 {
-    public static void main(String[] args) {
-        Lesson19 myobj = new Lesson19();
-        myobj.setName("Rizwan");
-        System.out.println(myobj.GetName());
-    }
-}
