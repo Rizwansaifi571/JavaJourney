da5bd15cfcc2884676c6227556d388479a7ba94d
rizwansaifi571 <rizwansaifi2614@gmail.com>
2024-10-28 19:50:44 +0530

    Concepts

---
+++ W3_Lessons/Lesson06.java
@@ -0,0 +1,23 @@
+public class Lesson06 {
+    public static void main(String[] args) {
+        // Java Math
+
+        // max and min function
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.min(5, 10));
+
+        // Square root function
+        System.out.println(Math.sqrt(64));
+
+        // absolute function
+        System.out.println(Math.abs(-645.4565));
+
+        // Random number between 0 to 1
+        System.out.println(Math.random());
+
+        // Random number between 0 to 100
+        System.out.println((int)(Math.random() * 101));
+        
+    }
+    
+}
